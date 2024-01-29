5e4f30534125ebd1d53b122fc99db0f8ab5dd3b6
Ben Iovino <90732453+biovino1@users.noreply.github.com>
2024-01-29 09:33:21 -0500

    added more explanation

--- test/commands.sh
+++ test/commands.sh
@@ -1,10 +1,9 @@
 #Inputs: 
 #  1. example.fasta -- sequence file
-#  2. example.list -- protein list
-#  3. example.pair -- pairs of proteins 
+#  2. example.pair -- pairs of proteins 
 
 #generate DCTdomain fingerprints
-bash ../src/fingerprint.sh example.fasta example.list example
+bash ../src/fingerprint.sh example.fasta example
 
 #calculate pairwise similarity (for the pairs given by --pair) using DCTdomain fingerprints
 python ../src/dct-sim.py --pair example.pair --dct example-dct.npz --output example-dctsim.txt
