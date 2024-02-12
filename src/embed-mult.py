ca2a902c263011098d97e951bd264ad0bba5fb5a
Ben Iovino <90732453+biovino1@users.noreply.github.com>
2024-02-12 14:54:52 -0500

    removed batchsize, simplified embedding functions

--- src/embed-mult.py
+++ src/embed-mult.py
@@ -1,7 +1,7 @@
-"""Embeds a FASTA file using multiple layers of ESM2_t33_650M_UR50D.
+"""Embeds a FASTA file using multiple layers of ESM2.
 
 Usage:
-    embed-mult.py --input <fasta> --npy <npy> [--gpu] [--batchsize <batchsize>] [--overwrite]
+    embed-mult.py --input <fasta> --npy <npy> [--gpu] [--overwrite]
 
 Yuzhen Ye, Indiana University, Nov 2023
 """
@@ -13,21 +13,20 @@ import torch
 import esm
 import numpy as np
 
-def readfasta(filename: str, batchsize: int) -> list:
-    """Read fasta file and return a list of (seqid, seq) tuples.
+def readfasta(filename: str) -> iter:
+    """Read fasta file and returns a generator of (seqid, sequence) tuples one at a time.
 
     Args:
         filename (str): path to fasta file
-        batchsize (int): number of seqs to read at a time
 
     Returns:
-        list of (seqid, seq) tuples
+        generator: (seqid, sequence) tuples
     """
     seqid, seqseq = [], []
     inf = open(filename, "r", encoding="utf-8")
     for aline in inf:
         if aline[0] == '>':
-            if len(seqid) == batchsize:
+            if len(seqid) == 1:
                 yield seqid, seqseq
                 seqid, seqseq = [], []
             seqid.append(aline[1:-1])
@@ -37,14 +36,13 @@ def readfasta(filename: str, batchsize: int) -> list:
     yield seqid, seqseq
     inf.close()
 
-def split_seq(seqseq: str, seqid: str, idx: int, maxlen: int, overlap: int) -> list:
+def split_seq(seqseq: str, seqid: str, maxlen: int, overlap: int) -> list:
     """Returns a list of (seqid, subseq) tuples, where subseq is a substring of seqseq[idx] of
     length maxlen.
 
     Args:
         seqseq (str): sequence
         seqid (str): sequence id
-        idx (int): index of sequence in seqseq
         maxlen (int): maximum length of subseq
         overlap (int): number of overlapping positions between adjacent subseqs
 
@@ -53,37 +51,11 @@ def split_seq(seqseq: str, seqid: str, idx: int, maxlen: int, overlap: int) -> l
     """
 
     embed = []
-    for i in range(0, len(seqseq[idx]), maxlen-overlap):
-        subseq = seqseq[idx][i:i+maxlen]
+    for i in range(0, len(seqseq), maxlen-overlap):
+        subseq = seqseq[i:i+maxlen]
         if len(subseq) > overlap:
-            embed.append((seqid[idx], subseq))
+            embed.append((seqid, subseq))
     return embed
-
-def split_emb(embed: list, batch_converter: esm.Alphabet, device: str, model: esm.pretrained):
-    """Returns a dictionary of embeddings and contacts for each subsequence in a list of
-    (seqid, subseq) tuples.
-
-    Args:
-        embed (list): list of (seqid, subseq) tuples
-        batch_converter (esm.Alphabet): ESM alphabet
-        device (str): device to use for embedding
-        model (esm.pretrained): ESM model
-
-    Returns:
-        dict: dictionary where key is type of embedding and value is a list of overlapping
-        embeddings or contacts
-    """
-
-    edata = {'e13': [], 'e25': [], 'ct': []}
-    for emb in embed:
-        _, _, batch_tokens = batch_converter([emb])  # batch_labels, batch_strs
-        batch_tokens = batch_tokens.to(device)
-        with torch.no_grad():
-            results = model(batch_tokens, repr_layers=[13, 25], return_contacts=True)
-        edata['e13'].append(results["representations"][13].cpu().numpy())
-        edata['e25'].append(results["representations"][25].cpu().numpy())
-        edata['ct'].append(results["contacts"].cpu().numpy())
-    return edata
 
 def avg_emb(embeds: list, overlap: int) -> np.ndarray:
     """Returns a single embedding by averaging and concatenating a list of embeddings.
@@ -97,7 +69,6 @@ def avg_emb(embeds: list, overlap: int) -> np.ndarray:
 
     full_emb = np.array([])
     for emb in embeds:
-        emb = emb[0][1:-1]
         if len(full_emb) == 0:
             full_emb = emb
             continue
@@ -162,46 +133,50 @@ def avg_ct(contacts: list, increase: int) -> np.ndarray:
 
     full_contacts = np.array([])
     for i, cont in enumerate(contacts):
-        cont = cont[0]
         if len(full_contacts) == 0:  # initialize full array
             full_contacts = cont
             continue
         full_contacts = combine_contacts(full_contacts, cont, inc = increase, times = i)
     return full_contacts
 
-def embed_batch(model: esm.pretrained, batch_tokens: torch.Tensor, data: list, npyfile: str):
-    """Embeds a batch of protein sequences using the given model.
+def embed_seq(model: esm.pretrained, batch_converter: esm.Alphabet, seqid: str, seqseq: str, device: str) -> dict:
+    """Returns a dictionary of embeddings and contacts for a single sequence.
 
     Args:
         model (esm.pretrained): ESM model
-        batch_tokens (torch.Tensor): batch of sequences to embed
-        data (list): list of (seqid, seq) tuples
-        npyfile (str): path to output file
+        batch_converter (esm.Alphabet): ESM tokenizer
+        seqid (str): sequence id
+        seqseq (str): sequence
+        device (str): device to use for embedding
+
+    Returns:
+        dict: dictionary where key is output label and value is the embedding/contact map
     """
 
-    # Extract per-residue representations (on CPU)
+    # Prepare inputs for embedding
+    data = [(seqid, seqseq)]
+    _, _, batch_tokens = batch_converter(data)  # batch_labels, batch_strs
+    batch_tokens = batch_tokens.to(device)
+
+    # Embed sequence and throw error if sequence is too long
     try:
         with torch.no_grad():
-            results = model(batch_tokens, repr_layers=[13, 25], return_contacts=True)
+            results = model(batch_tokens, repr_layers=[15, 21], return_contacts=True)
     except RuntimeError:
-        print("RuntimeError occured, try smaller --maxgpu")
-        return
-    r13a = results["representations"][13].cpu()
-    r25a = results["representations"][25].cpu()
-    cta = results["contacts"].cpu()
+        print("RuntimeError occured, try smaller --maxlen")
+        sys.exit()
 
-    # Save data into npz file
-    np.set_printoptions(threshold=sys.maxsize)
-    for (sid, seq), e13, e25, ct in zip(data, r13a, r25a, cta):
-        filen = npyfile + "/" + sid
-        np.savez_compressed(filen, s=seq, e13=e13, e25=e25, ct=ct)
+    # indexing [0][1:-1] removes the start and end tokens
+    r15a = results["representations"][15].cpu()[0][1:-1]
+    r21a = results["representations"][21].cpu()[0][1:-1]
+    cta = results["contacts"].cpu()[0]
+    return {'e15': r15a, 'e21': r21a, 'ct': cta}
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", help="FASTA file", required=True)
     parser.add_argument("--npy", help="save embeddings and contact to files under the given folder (one for each protein)", required=True)
-    parser.add_argument("--gpu", help="using gpu, otherwise using cpu", default=False, required=False)
-    parser.add_argument("--batchsize", help="batchsize; default 1", default=1, required=False)
+    parser.add_argument("--gpu", help="using gpu, otherwise using cpu", default=True, required=False)
     parser.add_argument("--overwrite", help="redo if npy exists; otherwise skip if exists", type=bool, default=False, required=False)
     parser.add_argument("--maxlen", help="max length of sequence to embed", type=int, default=1000, required=False)
     args = parser.parse_args()
@@ -209,12 +184,12 @@ def main():
     if not os.path.exists(args.npy):
         os.mkdir(args.npy)
 
-    print("load model...")
+    #print("load model...")
     torch.cuda.empty_cache()
     device = torch.device('cuda:0' if torch.cuda.is_available() and args.gpu else 'cpu')
 
     # Load ESM-2 model
-    model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
+    model, alphabet = esm.pretrained.esm2_t30_150M_UR50D()
     batch_converter = alphabet.get_batch_converter()
     model.eval()  # disables dropout for deterministic results
     model.to(device)
@@ -224,47 +199,44 @@ def main():
     with open(listfile, "w", encoding='utf8') as listf:
         listf.write('#protein\n')
 
-    fasta = readfasta(args.input, args.batchsize)
+    fasta = readfasta(args.input)
     while True:
 
-        # Read batch of sequences
+        # Read next sequence in file
         try:
             seqid, seqseq = next(fasta)
+            seqid, seqseq = seqid[0], seqseq[0]
         except StopIteration:
             break
 
         # For each sequence in batch, prepare inputs for embedding
-        data = []
-        for idx in range(len(seqid)):  #pylint: disable=C0200
-            filen = args.npy + "/" + seqid[idx] + ".npz"
-            with open(listfile, "a", encoding='utf8') as listf:  # add to .list file
-                listf.write(f"{seqid[idx]}\n")
-            if (not args.overwrite) and os.path.exists(filen):  # skip if file exists
-                print(f"seq {seqid[idx]} already exists")
-                continue
-
-            # Split sequence if too long and embed individually
-            if len(seqseq[idx]) > args.maxlen:
-                print(f"seq {seqid[idx]} too long {len(seqseq[idx])}, splitting")
-                overlap = 200
-                embed = split_seq(seqseq, seqid, idx, args.maxlen, overlap)
-                edata = split_emb(embed, batch_converter, device, model)
-                edata['e13'] = avg_emb(edata['e13'], overlap)
-                edata['e25'] = avg_emb(edata['e25'], overlap)
-                edata['ct'] = avg_ct(edata['ct'], args.maxlen-overlap)
-                np.savez_compressed(filen, s=seqseq[idx], e13=edata['e13'], e25=edata['e25'], ct=edata['ct'])
-                continue
-
-            # Add each sequence to list of sequences to embed
-            print(f"seq {seqid[idx]} {len(seqseq[idx])}")
-            data.append((seqid[idx], seqseq[idx]))
-            _, _, batch_tokens = batch_converter(data)  # batch_labels, batch_strs
-            batch_tokens = batch_tokens.to(device)
-        if len(data) == 0:  # skip if no inputs were processed
+        filen = args.npy + "/" + seqid + ".npz"
+        with open(listfile, "a", encoding='utf8') as listf:  # add to .list file
+            listf.write(f"{seqid}\n")
+        if (not args.overwrite) and os.path.exists(filen):  # skip if file exists
+            print(f"seq {seqid} already exists")
             continue
 
-        # Embed batch of sequences
-        embed_batch(model, batch_tokens, data, args.npy)
+        # Split sequence if too long and embed individually
+        if len(seqseq) > args.maxlen:
+            print(f"seq {seqid} too long {len(seqseq)}, splitting")
+            overlap = 200
+            edata = {'e15': [], 'e21': [], 'ct': []}
+            subseqs = split_seq(seqseq, seqid, args.maxlen, overlap)
+            for seq in subseqs:
+                embed = embed_seq(model, batch_converter, seq[0], seq[1], device)
+                for key in edata:  #pylint: disable=C0206
+                    edata[key].append(embed[key])
+            edata['e15'] = avg_emb(edata['e15'], overlap)
+            edata['e21'] = avg_emb(edata['e21'], overlap)
+            edata['ct'] = avg_ct(edata['ct'], args.maxlen-overlap)
+            np.savez_compressed(filen, s=seqseq, e15=edata['e15'], e21=edata['e21'], ct=edata['ct'])
+            continue
+
+        # Add each sequence to list of sequences to embed
+        print(f"seq {seqid} {len(seqseq)}")
+        edata = embed_seq(model, batch_converter, seqid, seqseq, device)
+        np.savez_compressed(filen, s=seqseq, e15=edata['e15'], e21=edata['e21'], ct=edata['ct'])
 
 if __name__ == "__main__":
     main()
