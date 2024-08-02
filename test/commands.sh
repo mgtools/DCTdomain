4f01d9ef1c45a78e78a0b3c1a4cf9608af0e1fac
Yuzhen Ye <yye@trojai2.luddy.indiana.edu>
2024-08-01 22:22:53 -0400

    did some clean-up

--- test/commands.sh
+++
@@ -1,9 +0,0 @@
-#Inputs: 
-#  1. example.fasta -- sequence file
-#  2. example.pair -- pairs of proteins 
-
-#generate DCTdomain fingerprints
-bash ../src/fingerprint.sh example.fasta example
-
-#calculate pairwise similarity (for the pairs given by --pair) using DCTdomain fingerprints
-python ../src/dct-sim.py --pair example.pair --dct example-dct.npz --output example-dctsim.txt
