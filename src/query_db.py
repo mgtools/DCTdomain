91e221ac6a90741cd4e29de37c6542b09ff763d6
Ben Iovino <90732453+biovino1@users.noreply.github.com>
2024-08-01 18:18:02 -0700

    sim metric

--- src/query_db.py
+++ src/query_db.py
@@ -54,8 +54,9 @@ def get_top_hits(dm: np.ndarray, im: np.ndarray, top: int, fp_db: Database, quer
         select = """ SELECT pid, domain FROM fingerprints WHERE vid = ? """
         db_pid, db_domain = fp_db.cur.execute(select, (db_vid+1,)).fetchone()
         q_pid, q_domain = query_db.cur.execute(select, (q_vid,)).fetchone()  # not taken from faiss
+        score = round(1-(top_hits[index]/17000), 4)  # similarity score
         logging.info('Query: %s %s, Result %s: %s %s, Similarity: %s',
-                      q_pid, q_domain, i+1, db_pid, db_domain, 1-(top_hits[index]/17000))
+                      q_pid, q_domain, i+1, db_pid, db_domain, score)
 
 
 def search_db(args: argparse.Namespace, query_db: str, fp_db: str, metric: str = 'l1'):
